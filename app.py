6a59c82fd7d74c36886e4e6c13b98a696522f1f8
malcolmfrsr <malcolmfrsr@gmail.com>
2019-04-23 01:07:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import time
 import redis
 from flask import Flask
 
-# got.
+# got.g
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
-# force.
+# force.g
 def get_hit_count():
     retries = 5
     while True:
